cbcf711d0256d217f2ce1e636c1ddca2cda717e2
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-06-16 22:12:54 +0100

    Removed some comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import theme from '../libs/theme';
 
 const MyApp = ({ Component, pageProps }) => {
 	useEffect(() => {
-		// Remove the server-side injected CSS.
 		const jssStyles = document.querySelector('#jss-server-side');
 		if (jssStyles) {
 			jssStyles.parentElement.removeChild(jssStyles);
