a4773b412c72c2585faab009481cbcc59ca079e0
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-06-19 14:50:34 +0100

    Fixed _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,8 @@ export default class MyDocument extends Document {
 						rel='stylesheet'
 						href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
 					/>
+					<link rel='manifest' href='/manifest.json' />
+					<link rel='icon' href='/favicon.ico' />
 				</Head>
 				<body>
 					<Main />
