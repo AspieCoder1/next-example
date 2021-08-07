7f5ef28f67d0c853ad1986a068ddd5e4b1b94512
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-08-07 16:54:34 +0100

    Set up accessing the user metadata in the user object

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -4,24 +4,9 @@ import { handleAuth, handleCallback, Session, GetLoginState } from '@auth0/nextj
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session: Session, state: GetLoginState) => {
-	// For the real app we basically just do a bit of code to get the correct namespace
-	// Probably using the node environment variable
-	let nameSpace = '';
-	switch (process.env.NODE_ENV) {
-		case 'development':
-			nameSpace = 'http://localhost:3000';
-			break;
-		case 'test':
-			nameSpace = 'http://localhost:3000';
-			break;
-		case 'production':
-			nameSpace = 'https://next-example-eta.vercel.app';
-			break;
-	}
-
+	let nameSpace = 'https://next-example-eta.vercel.app';
 	session.user = { ...session.user, user_data: session.user[`${nameSpace}/user_data`] || {} };
 	delete session.user[`${nameSpace}/user_data`];
-	console.log(session);
 	return session;
 };
 
