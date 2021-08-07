8a767bdb87799776ce94b7488bc6d7bd89576fdb
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-08-07 16:50:36 +0100

    Set up so that we have seperate namespaces for each environment

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -4,8 +4,23 @@ import { handleAuth, handleCallback, Session, GetLoginState } from '@auth0/nextj
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session: Session, state: GetLoginState) => {
-	session = { ...session, user_data: session['http://next-example-eta.vercel.app/user_data'] };
-	delete session['http://next-example-eta.vercel.app/user_data'];
+	// For the real app we basically just do a bit of code to get the correct namespace
+	// Probably using the node environment variable
+	let nameSpace = '';
+	switch (process.env.NODE_ENV) {
+		case 'development':
+			nameSpace = 'http://localhost:3000';
+			break;
+		case 'test':
+			nameSpace = 'http://localhost:3000';
+			break;
+		case 'production':
+			nameSpace = 'https://next-example-eta.vercel.app';
+			break;
+	}
+
+	session.user = { ...session.user, user_data: session.user[`${nameSpace}/user_data`] || {} };
+	delete session.user[`${nameSpace}/user_data`];
 	console.log(session);
 	return session;
 };
